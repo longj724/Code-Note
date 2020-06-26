6829cefb9463f506d60f24bd802afb2725434224
juslong <juslong@umich.edu>
2020-06-25 23:01:23 -0400

    material-ui theme

---
+++ frontend/src/CSS/theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    palette: {
+        action: {
+            hover: '#bdbdbd',
+            selected: '#fff'
+        },
+        primary: {
+            light: '#1F1F1F',
+            main: '#1414141',
+            dark: '#0A0A0A'
+        }
+    },
+    typography: {
+        fontFamily: 'Roboto',
+        h6: {
+            fontWeight: 'bold'
+        }
+    }
+})
+
+export default theme
