82952bef6c4edc93ce219d98a605e3d1aae9f990
juslong <juslong@umich.edu>
2020-08-11 21:42:38 -0400

    Changing directory structure for Heroku

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ server.use(bodyParser.json());
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('../frontend/build'))
+    app.use(express.static('frontend/build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
