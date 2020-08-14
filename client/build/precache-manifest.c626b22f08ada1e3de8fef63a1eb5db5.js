72954db3df74afb0516a6e693427335f62e0edb0
juslong <juslong@umich.edu>
2020-08-13 22:55:26 -0400

    more .gitignore fixes

---
+++ client/build/precache-manifest.c626b22f08ada1e3de8fef63a1eb5db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e796823dd1e963a4078dca2f03fb2fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "969fb9c16d5a98aeca91",
+    "url": "/static/css/main.2e207d4c.chunk.css"
+  },
+  {
+    "revision": "de406bbf56a9d1240ff5",
+    "url": "/static/js/2.8be691b3.chunk.js"
+  },
+  {
+    "revision": "0adb0005e110cb0d8cc5b4e3b6aa7b57",
+    "url": "/static/js/2.8be691b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969fb9c16d5a98aeca91",
+    "url": "/static/js/main.c975050d.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
