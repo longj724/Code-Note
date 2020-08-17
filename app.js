79a1cfccfb060a4401b91bc0d79581db362cb658
juslong <juslong@umich.edu>
2020-08-17 14:06:56 -0400

    Updating app.js

--- app.js
+++ app.js
@@ -52,10 +52,10 @@ server.use(bodyParser.json());
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('frontend/build'))
+    app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
